42f6db3386663ef6fdf0833c98efda81f0190bd5
Matthijs Lavrijsen <mattiwatti@gmail.com>
2024-03-12 00:19:49 +0100

    Update to VS2022
    - Disable LTO explicitly (otherwise VS2019 and up may be tempted to use /LTCG:FAST, which breaks the output exe)
    - Disable ASLR for 64 bit executable as well
    - Pass /MERGE:.rdata=.text on 64 bit as a temporary workaround for an added bogus section. The real fix is to recreate main.c from the .asm files, which do assemble and link correctly using the VS2022 linker.

--- asm/make.cmd
+++ asm/make.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-set VSVARS32=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars32.bat
-set VSVARS64=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat
+set VSVARS32=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat
+set VSVARS64=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat
 set NASMDIR=C:\Nasm
 
 setlocal
@@ -16,12 +16,13 @@ mkdir obj64 1>nul 2>&1
 mkdir bin32 1>nul 2>&1
 mkdir bin64 1>nul 2>&1
 
-:: 32 bit. Note: /DYNAMICBASE:NO is used to avoid a reloc directory (which can't go in the same .text section as main)
+:: Note: /DYNAMICBASE:NO is used to avoid a reloc directory (which can't go in the same .text section as main)
+:: 32 bit
 call "%VSVARS32%" 1>nul
 set PATH=%NASMDIR%;%PATH%
 
 nasm -fwin32 -DWINDOWS -D_WINDOWS -Worphan-labels -o obj32/main.obj main32.asm
-link /nologo /OUT:"bin32\WinMain.exe" /MANIFEST:NO /NXCOMPAT /NOCOFFGRPINFO /DYNAMICBASE:NO /RELEASE /VERSION:"5.01" /MACHINE:X86 /SAFESEH /INCREMENTAL:NO /OPT:REF /OPT:ICF /SUBSYSTEM:WINDOWS",5.01" "obj32\main.obj" /NODEFAULTLIB
+link /nologo /OUT:"bin32\WinMain.exe" /MANIFEST:NO /NXCOMPAT /NOVCFEATURE /NOCOFFGRPINFO /DYNAMICBASE:NO /MERGE:.rdata=.text /RELEASE /VERSION:"5.01" /MACHINE:X86 /SAFESEH /INCREMENTAL:NO /OPT:REF /OPT:ICF /SUBSYSTEM:WINDOWS",5.01" "obj32\main.obj" /NODEFAULTLIB
 
 :: 64 bit
 set PATH=%OLDPATH%
@@ -29,7 +30,7 @@ call "%VSVARS64%" 1>nul
 set PATH=%NASMDIR%;%PATH%
 
 nasm -fwin64 -DWINDOWS -D_WINDOWS -Worphan-labels -o obj64/main.obj main64.asm
-link /nologo /OUT:"bin64\WinMain.exe" /MANIFEST:NO /NXCOMPAT /NOCOFFGRPINFO /DYNAMICBASE /RELEASE /VERSION:"5.02" /MACHINE:X64 /INCREMENTAL:NO /OPT:REF /OPT:ICF /SUBSYSTEM:WINDOWS",5.02" "obj64\main.obj" /NODEFAULTLIB
+link /nologo /OUT:"bin64\WinMain.exe" /MANIFEST:NO /NXCOMPAT /NOVCFEATURE /NOCOFFGRPINFO /DYNAMICBASE:NO /MERGE:.rdata=.text /RELEASE /VERSION:"5.02" /MACHINE:X64 /INCREMENTAL:NO /OPT:REF /OPT:ICF /SUBSYSTEM:WINDOWS",5.02" "obj64\main.obj" /NODEFAULTLIB
 
 set PATH=%OLDPATH%
 endlocal
